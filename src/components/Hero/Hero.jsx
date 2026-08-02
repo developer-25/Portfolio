e0e7ebd849921a6f0d045f3dd737406817974212
Kalash Mantri <kalashmantri2002@gmail.com>
2026-08-02 15:56:21 +0530

    photo wrong

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -71,7 +71,7 @@ const Hero = ({ darkMode }) => {
         {/* Right Content (Image) */}
         <div className="flex justify-center flex-1">
           <motion.img
-            src="/kalash photo.jpeg"
+            src="/ChatGPT Image Apr 30, 2025, 11_21_25 PM.png"
             alt="Profile"
             className="w-48 sm:w-56 md:w-64 lg:w-80 rounded-full border-4 shadow-lg 
             transition-all duration-300 
